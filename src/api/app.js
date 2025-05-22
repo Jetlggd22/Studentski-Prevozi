892960f73a8cc4127c3f24fe29a030f230a3c36a
dejanovic1108 <dejanovic1108@gmail.com>
2025-05-22 22:36:09 +0200

    koncal zgodovino za admina

--- src/api/app.js
+++ src/api/app.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes.js';
-import cors from 'cors'
-import path from 'path'; 
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url'; // Dodano
+
+// Pravilna definicija __dirname za ES module
+const __filename = fileURLToPath(import.meta.url); // Dodano
+const __dirname = path.dirname(__filename); // Dodano
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(process.cwd(), 'frontend')));
+app.use(
+  // Pot do mape 'frontend' je sedaj relativna na trenutno datoteko 'app.js'
+  express.static(path.join(__dirname, '..', 'frontend'))
+);
 app.use('/api', routes);
 
 // basic error handler
