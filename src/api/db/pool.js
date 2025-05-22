14263444c951e71f05d4fe463ce21520c9d7e18b
pavelBozinov333 <pavelBozinov333@gitlab.com>
2025-05-22 22:28:11 +0200

    added ride details in frontend and backend

--- src/api/db/pool.js
+++ src/api/db/pool.js
@@ -5,7 +5,7 @@ import mysql from "mysql2/promise"
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: 'your_root_password',
   database: 'studentski_prevoz'
 });
 
--- src/api/services/rideService.js
+++ src/api/services/rideService.js
@@ -1,5 +1,9 @@
 import * as prevozRepository from '../repositories/rideRepository.js';
 
+export async function getPrevozById(id) {
+  return await prevozRepository.findPrevozById(id);
+}
+
 export async function listPrevozi() {
   const prevozi = await prevozRepository.getAllPrevozi();
   return prevozi;
